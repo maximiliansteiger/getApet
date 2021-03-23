d7b65b6c1da68612827c76461f284d50cec2f673
maximiliansteiger <maximiliansteiger15@gmail.com>
2021-03-23 10:15:20 +0100

    Login and sign up design

--- project/app/src/main/java/com/example/getapet_login/ui/login/LoginActivity.java
+++ project/app/src/main/java/com/example/getapet_login/ui/login/LoginActivity.java
@@ -23,8 +23,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.getapet_login.R;
-import com.example.getapet_login.ui.login.LoginViewModel;
-import com.example.getapet_login.ui.login.LoginViewModelFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -39,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
 
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
+        final Button loginButton = findViewById(R.id.signup);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
