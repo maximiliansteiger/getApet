6a46f3fbae11be1a42bef9cc3b39fb95c92c6e8b
maximiliansteiger <maximiliansteiger15@gmail.com>
2021-04-02 17:23:51 +0200

    changed main page

---
+++ project/app/src/main/java/com/example/getapet_login/accountPage.java
@@ -0,0 +1,14 @@
+package com.example.getapet_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class accountPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_page);
+    }
+}
---
+++ project/app/src/main/java/com/example/getapet_login/addAnimalPage.java
@@ -0,0 +1,14 @@
+package com.example.getapet_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addAnimalPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_animal_page);
+    }
+}
